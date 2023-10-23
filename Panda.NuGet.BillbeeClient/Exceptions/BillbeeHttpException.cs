301052e39b976a07fabd8d4bda1069ea77b419a7
Patrick Knäpel <patrick.knaepel@pandaliebe.de>
2023-10-23 12:06:52 +0200

    refactor(Exceptions) C# 8 Nullable types

--- Panda.NuGet.BillbeeClient/Exceptions/BillbeeHttpException.cs
+++ Panda.NuGet.BillbeeClient/Exceptions/BillbeeHttpException.cs
@@ -4,14 +4,14 @@ namespace Panda.NuGet.BillbeeClient.Exceptions;
 
 public class BillbeeHttpException : Exception
 {
-    public BillbeeHttpException(string caller, HttpStatusCode statusCode, string contentStr) : base(
+    public BillbeeHttpException(string caller, HttpStatusCode statusCode, string? contentStr) : base(
         $"Panda.NuGet.BillbeeClient | {caller} - Unexpected HTTP Status Code: " + statusCode)
     {
         StatusCode = statusCode;
         ContentStr = contentStr;
     }
 
-    public string ContentStr { get; set; }
+    public string? ContentStr { get; set; }
 
     public HttpStatusCode StatusCode { get; set; }
 }
