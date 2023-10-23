fbdc46ea90ac6340986eb0f0a45b6d5dcc4aaea8
Patrick Knäpel <patrick.knaepel@pandaliebe.de>
2023-10-23 12:06:38 +0200

    refactor(Configs) C# 8 Nullable types

--- Panda.NuGet.BillbeeClient/Configs/BillbeeApiConfig.cs
+++ Panda.NuGet.BillbeeClient/Configs/BillbeeApiConfig.cs
@@ -10,21 +10,21 @@ namespace Panda.NuGet.BillbeeClient.Configs
         /// <summary>
         /// Username of the main user of your account.
         /// </summary>
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
         /// <summary>
         /// API password, could be set in Settings->Billbee API->General Settings
         /// </summary>
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         /// <summary>
         /// The Api key for your application. Can be requested from the Billbee support.
         /// </summary>
-        public string ApiKey { get; set; }
+        public string? ApiKey { get; set; }
 
         /// <summary>
         /// The base url of the Billbee API. Typically, this sticks unchanged.
         /// </summary>
-        public string BaseUrl { get; set; } = "https://app.billbee.io/api/v1";
+        public string? BaseUrl { get; set; } = "https://app.billbee.io/api/v1";
     }
 }
